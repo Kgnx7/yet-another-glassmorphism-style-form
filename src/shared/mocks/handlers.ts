406eff8173c28b49b5327632f59a339df8a56668
Daniel Ilich <ukqfhf@Daniels-MacBook-Pro.local>
2023-05-04 00:33:07 +0800

    add base to vite.config.ts

--- src/shared/mocks/handlers.ts
+++ src/shared/mocks/handlers.ts
@@ -2,7 +2,7 @@ import { rest } from "msw";
 
 export const handlers = [
   // Handles a POST /login request
-  rest.post("/login", (req, res, ctx) => {
+  rest.post("/login", (_req, res, ctx) => {
     return res(
       // Respond with a 200 status code
       ctx.status(200)
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/yet-another-glassmorphism-style-form/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
